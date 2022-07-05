5e6cd7f7982baf1dd8a6aaa46ee969c89044dac1
Milk Steak <a.khatttab97@gmail.com>
2022-07-05 17:36:50 +0200

    Get the first three mode to work

--- script.js
+++ script.js
@@ -3,19 +3,20 @@ let range = document.getElementById('range');
 let value = range['value'];
 para.textContent = `${value} X ${value}`;
 
-let grid = document.getElementsByClassName('grid')[0];
+let grid = document.querySelector('.grid');
 let colorbtn = document.getElementById('color-mode');
 let rainbowbtn = document.getElementById('rainbow-color');
 let erase = document.getElementById('erase');
 let clear = document.getElementById('clear');
 let color = document.getElementById('color')['value'];
 console.log(color);
+let currentMode = 'color';
 
 let clicked = 1;
 
-colorbtn.addEventListener('click', () => paint());
-rainbowbtn.addEventListener('click', () => clicked=2);
-erase.addEventListener('click', () => clicked=3);
+colorbtn.addEventListener('click', () => setMode('color'));
+rainbowbtn.addEventListener('click', () => setMode('rainbow'));
+erase.addEventListener('click', () => setMode('erase'));
 clear.addEventListener('click', () => clicked=4);
 
 
@@ -27,7 +28,10 @@ function createGrid(grid) {
         // flex element
         for (let i = 0; i < value; i++) {
             let flex_element = document.createElement('div');
-            flex_element.style.cssText = "flex-grow: 1; background-color: #808080; border-color: #000000; height:25px";
+            flex_element.style.cssText = "flex-grow: 1; background-color: #808080";
+            flex_element.style['height'] = `${grid.clientHeight/value}px`;
+            flex_element.addEventListener('mousedown', colorPicker);
+            flex_element.addEventListener('mouseover', colorPicker)
             flex_element.classList.add('flex-item');
             div_wrap.appendChild(flex_element);
         }
@@ -36,17 +40,26 @@ function createGrid(grid) {
 }
 
 createGrid(grid);
-let flex_items = Array.from(document.querySelectorAll('.flex-item'));
-console.log(flex_items);
 
-function paint(){
-    flex_items.forEach(flex_item =>{
-        flex_item.addEventListener('mousedown', colorPicker)
-    });
+
+let startDraw = false;
+document.body.onmousedown = () => startDraw = true;
+document.body.onmouseup = () => startDraw = false;
+
+function colorPicker(e) {
+    if (e.type === 'mouseover' && !startDraw) return;
+    if (currentMode === 'rainbow') {
+        const randomR = Math.floor(Math.random() * 256)
+        const randomG = Math.floor(Math.random() * 256)
+        const randomB = Math.floor(Math.random() * 256)
+        e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+    } else if (currentMode === 'color') {
+        e.target.style.backgroundColor = color
+    } else if (currentMode === 'erase') {
+        e.target.style.backgroundColor = "#808080";
+    }
 }
 
-function colorPicker(e){
-    console.log(e.target);
-    console.log(color);
-    e.target.style.backgroundColor = color;
+function setMode(mode='color'){
+    currentMode = mode;
 }
